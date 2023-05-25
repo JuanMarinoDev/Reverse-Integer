f0f98afa9daa237519b07357a2bcbb146a903e9b
juan <www.juangui087@hotmail.com>
2023-05-25 00:39:49 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,53 @@
+/*_________________________________________________________*/
+/*-----------      Reverse Number       -----------*/
+/*-------------------------------------------------------*/
+/*
+*/
+
+
+function reverse(num) {
+  let str = num.toString();
+  let isNegative = false;
+  let revNum = "";
+  str = str.split('');
+  str.reverse();
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== '-') {
+      revNum = revNum.concat(str[i]);
+    } else {
+      isNegative = true;
+    }
+  }
+  if (isNegative === true) {
+    revNum = '-' + revNum;
+  }
+  revNum = parseInt(revNum);
+  if (revNum < -2147483648 || revNum > 2147483647) {
+    revNum = 0;
+  }
+  console.log(revNum);
+};
+
+reverse(1534236469);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
